e65d59094b7dcebee23b0671fcd74dcbcabecf92
Isaac Hunter <ikehunter5@gmail.com>
2025-03-03 18:41:23 -0500

    format code

--- app/dashboard/views.py
+++ app/dashboard/views.py
@@ -1,11 +1,7 @@
-from django.http import JsonResponse
 from django.shortcuts import render
-from django.utils import timezone
-from rest_framework.response import Response
-from rest_framework.status import HTTP_400_BAD_REQUEST
-from rest_framework.views import exception_handler
 
 # Create your views here.
+
 
 def index(request):
     context = {}
