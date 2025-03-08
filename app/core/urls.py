ea94ccf613cb513359ec683924a45e7bb2d17837
Isaac Hunter <ikehunter5@gmail.com>
2025-03-07 22:33:43 -0500

    fix api network auth

--- app/core/urls.py
+++ app/core/urls.py
@@ -7,5 +7,4 @@ app_name = "core"
 urlpatterns = [
     path("", views.index, name="index"),
     path("health/", views.health_check, name="health"),
-    path("styles/", views.styles, name="styles"),
 ]
