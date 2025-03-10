8f0b6354f00eb28cb9387df806bc174395bb2705
Isaac Hunter <ikehunter5@gmail.com>
2025-03-10 19:14:35 -0400

    fix user token api

--- app/users/viewsets.py
+++ app/users/viewsets.py
@@ -2,11 +2,13 @@
 Views for the user API.
 """
 
-from rest_framework import authentication, generics
+from rest_framework import authentication, generics, mixins
+from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.response import Response
 from rest_framework.settings import api_settings
 
-from core.abstracts.viewsets import ModelViewSetBase
+from core.abstracts.viewsets import ModelViewSetBase, ViewSetBase
 from users.serializers import UserSerializer
 
 
@@ -16,7 +18,11 @@ class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
 
 
-class AuthTokenView(ObtainAuthToken, generics.RetrieveAPIView, generics.GenericAPIView):
+class AuthTokenView(
+    ObtainAuthToken,
+    mixins.RetrieveModelMixin,
+    ViewSetBase,
+):
     """Create a new auth token for user."""
 
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
@@ -24,6 +30,37 @@ class AuthTokenView(ObtainAuthToken, generics.RetrieveAPIView, generics.GenericA
         authentication.TokenAuthentication,
         authentication.SessionAuthentication,
     ]
+
+    def retrieve(self, request, *args, **kwargs):
+        # The default doesn't save request, so need to do that manually
+        token, _ = Token.objects.get_or_create(user=request.user)
+        return Response({"token": token.key})
+
+
+# class CreateTokenView(ObtainAuthToken):
+#     """Create a new auth token for user."""
+
+#     serializer_class = AuthTokenSerializer
+#     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+#     authentication_classes = [
+#         authentication.TokenAuthentication,
+#         authentication.SessionAuthentication,
+#     ]
+
+
+# class RetrieveTokenView(mixins.RetrieveModelMixin, ViewSetBase):
+#     """Separate view for obtaining token from session."""
+
+#     serializer_class = AuthTokenSerializer
+#     authentication_classes = [
+#         authentication.TokenAuthentication,
+#         authentication.SessionAuthentication,
+#     ]
+
+#     def get_object(self):
+
+#         token, _ = Token.objects.get_or_create(user=self.request.user)
+#         return token
 
 
 class ManageUserView(generics.RetrieveUpdateAPIView):
